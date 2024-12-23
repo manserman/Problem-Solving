05a3cb4d0dbbc2c2566cefa99a4c5916b4747d87
Mansour HABIBOU <73804349+manserman@users.noreply.github.com>
2024-12-23 13:15:33 +0100

    Update ProductExceptSelf.java

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -1,21 +1,32 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        
-        // Calcul des préfixes
-        result[0] = 1;
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+        int total=1;
+        int nbZero=0;
+        for(int i=0;i<nums.length;i++){
+           
+            if(nums[i]!=0){
+            total*=nums[i];
+            }
+            else
+            nbZero++;
+             if(nbZero>1){
+                total=0;
+                break;
+            }
         }
-        
-        // Calcul des suffixes et produit final
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= suffix;
-            suffix *= nums[i];
+        int[] result= new int[nums.length];
+         for(int i=0;i<nums.length;i++){
+            int n=total;
+           if(nbZero>=1){
+            if(nums[i]!=0)
+               n=0;
+           }
+           else
+            n=total/nums[i];
+            
+            result[i]=n;
         }
-        
         return result;
+        
     }
 }
